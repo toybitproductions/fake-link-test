0b7dbbc65afc0884a058a61b73276af33bd24736
toybitproductions <toybitofc@gmail.com>
2025-05-17 14:16:48 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "mySong":return hex`00780004080200`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
